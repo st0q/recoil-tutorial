20075611d06bbd07d7d59030efd95c9efe04bc80
st0q <tssk713@gmail.com>
2023-07-05 23:35:06 +0900

    add filter state

--- src/common/recoilAtomKeys.ts
+++ src/common/recoilAtomKeys.ts
@@ -1,3 +1,9 @@
+// A unique string used to identify the selector internally. This string should be unique with respect to other atoms and selectors in the entire application.
+// https://recoiljs.org/docs/api-reference/core/selector/
 export const atomKeys = {
-  TODO_LIST: "TodoList",
-} as const;
+  TODO_LIST: "TodoListAtom",
+  TODO_LIST_FILTER: "TodoListFilterAtom",
+} as const satisfies {
+  // selector の key と重複しないように値は Atom で終わる文字列に限定する
+  [key: string]: `${string}Atom`;
+};
--- src/features/todos/state.ts
+++ src/features/todos/state.ts
@@ -7,7 +7,14 @@ export type Todo = {
   isComplete: boolean;
 };
 
+export type TodoListFilter = "Show All" | "Show Completed" | "Show Uncompleted";
+
 export const todoListState = atom<Todo[]>({
   key: atomKeys.TODO_LIST,
   default: [],
+});
+
+export const todoListFilterState = atom<TodoListFilter>({
+  key: atomKeys.TODO_LIST_FILTER,
+  default: "Show All",
 });
